d4ea4421409f8f317af00332777f313e950d8f6b
Seonghyeon Lee <Seonghyeon.Lee@cognex.com>
2024-04-02 14:20:05 +0900

    Feat: "서버 인터페이스 추가"

--- Server/CommunicationServer/CommunicationServer/Protocols/IServer.cs
+++ Server/CommunicationServer/CommunicationServer/Protocols/IServer.cs
@@ -8,6 +8,10 @@ namespace CommunicationServer.Protocols
 {
     public interface IServer
     {
-
+        bool IsRunning();
+        Action<string> RecieveMessage { get; set; }
+        void On();
+        void Off();
+        void Send(string data);
     }
 }

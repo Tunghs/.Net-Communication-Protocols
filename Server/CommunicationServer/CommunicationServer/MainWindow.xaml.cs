4fef09df97d553687944935ff36cd9e588460c86
Tunghs <juk08843@gmail.com>
2024-04-02 14:19:32 +0900

    Feat: "GUI 업데이트"

--- Server/CommunicationServer/CommunicationServer/MainWindow.xaml.cs
+++ Server/CommunicationServer/CommunicationServer/MainWindow.xaml.cs
@@ -1,70 +1,40 @@
-﻿using CommunicationServer.Protocols;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CommunicationServer
 {
+    public enum CommunicationMode
+    {
+        TcpIp,
+        REST,
+        IPC,
+        WCF
+    }
+
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<CommunicationView> views;
+        List<TabItem> tabs;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
         }
 
-        private void SendBtn_Click(object sender, RoutedEventArgs e)
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(MessageTbx.Text))
+            views = new List<CommunicationView>() { TcpIpComuView, RestComuView, IpcComuView, WcfComuView };
+            tabs = new List<TabItem>() { TcpIPTab, RestTab, IpcTab, WcfTab };
+            for (int index = 0; index < 4; index++)
             {
-                return;
-            }
-
-            UpdateLog(MessageTbx.Text, true);
-            MessageTbx.Text = string.Empty;
-        }
-
-        private void UpdateLog(string message, bool isServer)
-        {
-            Application.Current.Dispatcher.BeginInvoke(new Action(delegate ()
-            {
-                
-                if (isServer)
-                {
-                    message = "Server: " + message;
-                }
-                else
-                {
-                    message = "Client: " + message;
-                }
-
-                LogTbx.AppendText(message);
-                LogTbx.AppendText(Environment.NewLine);
-                LogTbx.ScrollToEnd();
-            }));
-        }
-
-        private void RestApiBtn_Click(object sender, RoutedEventArgs e)
-        {
-            if (RestApiBtn.IsChecked.Value)
-            {
-                RestApiServer api = new RestApiServer();
-                api.UpdatedMessage += ((m) => UpdateLog(m, false));
-                api.Initialize();
+                views[index].Intialize((CommunicationMode)index);
+                views[index].OpenServer += (() => { tabs.ForEach(x => x.IsEnabled = false); tabs[ServerTab.SelectedIndex].IsEnabled = true; ServerTab.Items.Refresh(); });
+                views[index].CloseServer += (() => { tabs.ForEach(x => x.IsEnabled = true); ServerTab.Items.Refresh(); });
             }
         }
     }

2771d41c05d31711597dcbfd5b888b3f1875d3f4
Tunghs <juk08843@gmail.com>
2024-04-03 11:16:17 +0900

    Feat: "Client UI 수정"
    * Server UI와 동일하게 변경

---
+++ Client/CommunicationClient/CommunicationClient/CommunicationView.xaml.cs
@@ -0,0 +1,96 @@
+﻿using CommunicationClient.Protocols;
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace CommunicationClient
+{
+    /// <summary>
+    /// CommunicationView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class CommunicationView : UserControl
+    {
+        #region Event
+        public Action OpenServer;
+        public Action CloseServer;
+        #endregion
+
+        #region Properties
+        public IClient Client { get; set; }
+        #endregion
+
+        public CommunicationView()
+        {
+            InitializeComponent();
+        }
+
+        public void Intialize(CommunicationMode mode)
+        {
+            switch (mode)
+            {
+                case CommunicationMode.TcpIp:
+                    Client = new IpcClient();
+                    break;
+                case CommunicationMode.REST:
+                    Client = new IpcClient();
+                    break;
+                case CommunicationMode.IPC:
+                    Client = new IpcClient();
+                    break;
+                case CommunicationMode.WCF:
+                    Client = new IpcClient();
+                    break;
+            }
+
+            Client.RecieveMessage += RecieveServerMessage;
+        }
+
+        private void ServerTbtn_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleButton button = (ToggleButton)sender;
+            if (button.IsChecked.Value)
+            {
+                Client.On();
+                if (Client.IsRunning())
+                    OpenServer?.Invoke();
+            }
+            else
+            {
+                Client.Off();
+                if (!Client.IsRunning())
+                    CloseServer?.Invoke();
+            }
+        }
+
+        private void SendBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(MessageTbx.Text))
+            {
+                return;
+            }
+
+            string message = MessageTbx.Text;
+            Client.Send(message);
+            ShowMessage("Client --> " + "msg");
+
+            MessageTbx.Text = string.Empty;
+        }
+
+        private void RecieveServerMessage(string message)
+        {
+            ShowMessage("Server --> " + message);
+        }
+
+        private void ShowMessage(string message)
+        {
+            Application.Current.Dispatcher.BeginInvoke(new Action(delegate ()
+            {
+                LogTbx.AppendText(message);
+                LogTbx.AppendText(Environment.NewLine);
+                LogTbx.ScrollToEnd();
+            }));
+        }
+    }
+}
--- Client/CommunicationClient/CommunicationClient/MainWindow.xaml.cs
+++ Client/CommunicationClient/CommunicationClient/MainWindow.xaml.cs
@@ -18,68 +18,92 @@ using System.Windows.Shapes;
 
 namespace CommunicationClient
 {
+    public enum CommunicationMode
+    {
+        TcpIp,
+        REST,
+        IPC,
+        WCF
+    }
+
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<CommunicationView> views;
+        List<TabItem> tabs;
+
         private RestAPI _client;
 
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
         }
 
-        private void SendBtn_Click(object sender, RoutedEventArgs e)
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(MessageTbx.Text))
+            views = new List<CommunicationView>() { TcpIpComuView, RestComuView, IpcComuView, WcfComuView };
+            tabs = new List<TabItem>() { TcpIPTab, RestTab, IpcTab, WcfTab };
+            for (int index = 0; index < 4; index++)
             {
-                return;
-            }
-
-            if (RestApiBtn.IsChecked.Value)
-            {
-                SendPostMsg();
-            }
-            
-            MessageTbx.Text = string.Empty;
-        }
-
-        private async void SendPostMsg()
-        {
-            UpdateLog(MessageTbx.Text, false);
-            var values =
-                new Dictionary<string, string> { { "username", "myUser" }, { "password", "myPassword" } };
-
-            await _client.PostAsync(values);
-        }
-
-        private void UpdateLog(string message, bool isServer)
-        {
-            Application.Current.Dispatcher.BeginInvoke(new Action(delegate ()
-            {
-
-                if (isServer)
-                {
-                    message = "Server: " + message;
-                }
-                else
-                {
-                    message = "Client: " + message;
-                }
-
-                LogTbx.AppendText(message);
-                LogTbx.AppendText(Environment.NewLine);
-                LogTbx.ScrollToEnd();
-            }));
-        }
-
-        private void RestApiBtn_Click(object sender, RoutedEventArgs e)
-        {
-            if (RestApiBtn.IsChecked.Value)
-            {
-                _client = new RestAPI();
+                views[index].Intialize((CommunicationMode)index);
+                views[index].OpenServer += (() => { tabs.ForEach(x => x.IsEnabled = false); tabs[ServerTab.SelectedIndex].IsEnabled = true; ServerTab.Items.Refresh(); });
+                views[index].CloseServer += (() => { tabs.ForEach(x => x.IsEnabled = true); ServerTab.Items.Refresh(); });
             }
         }
+
+        //private void SendBtn_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (string.IsNullOrEmpty(MessageTbx.Text))
+        //    {
+        //        return;
+        //    }
+
+        //    if (RestApiBtn.IsChecked.Value)
+        //    {
+        //        SendPostMsg();
+        //    }
+
+        //    MessageTbx.Text = string.Empty;
+        //}
+
+        //private async void SendPostMsg()
+        //{
+        //    UpdateLog(MessageTbx.Text, false);
+        //    var values =
+        //        new Dictionary<string, string> { { "username", "myUser" }, { "password", "myPassword" } };
+
+        //    await _client.PostAsync(values);
+        //}
+
+        //private void UpdateLog(string message, bool isServer)
+        //{
+        //    Application.Current.Dispatcher.BeginInvoke(new Action(delegate ()
+        //    {
+
+        //        if (isServer)
+        //        {
+        //            message = "Client: " + message;
+        //        }
+        //        else
+        //        {
+        //            message = "Client: " + message;
+        //        }
+
+        //        LogTbx.AppendText(message);
+        //        LogTbx.AppendText(Environment.NewLine);
+        //        LogTbx.ScrollToEnd();
+        //    }));
+        //}
+
+        //private void RestApiBtn_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (RestApiBtn.IsChecked.Value)
+        //    {
+        //        _client = new RestAPI();
+        //    }
+        //}
     }
 }
